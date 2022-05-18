9c753a5290939ec8657f564c4654a0dab9f144ab
NicholasYe <2216393769@qq.com>
2022-05-19 00:32:15 +0800

    5.19_12:32

---
+++ 2 Building Abstractions with Data/2.3 Sequences/Sequence Abstraction.py
@@ -0,0 +1,8 @@
+digits = [1, 8, 2, 8]
+Bool_1 = 2 in digits
+Bool_2 = 3 not in digits
+print('the bool_1 is:', Bool_1)
+print('the bool_2 is:', Bool_2)
+
+print(digits[0:2])
+print(digits[1:])
---
+++ 2 Building Abstractions with Data/2.3 Sequences/Trees.py
@@ -0,0 +1,7 @@
+one_two = [1, 2]
+nested = [[1, 2], [],
+          [[3, False, None],
+           [4, lambda: 5]]]
+
+print(one_two)
+print(nested)
