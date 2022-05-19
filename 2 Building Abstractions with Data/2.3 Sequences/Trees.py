c1bf4bb858a04d3f902e1eb0ccad4282fa27da7c
NicholasYe <2216393769@qq.com>
2022-05-19 11:01:09 +0800

    5.19_11:01

--- 2 Building Abstractions with Data/2.3 Sequences/Trees.py
+++ 2 Building Abstractions with Data/2.3 Sequences/Trees.py
@@ -1,7 +1,48 @@
-one_two = [1, 2]
-nested = [[1, 2], [],
-          [[3, False, None],
-           [4, lambda: 5]]]
+def tree(root_label, branches=[]):
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [root_label] + list(branches)
 
-print(one_two)
-print(nested)
+def label(tree):
+    return tree[0]
+
+def branches(tree):
+    return tree[1:]
+
+def is_tree(tree):
+    """
+    Verify that all branches are well-formed
+    """    
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+def is_leaf(tree):
+    """
+    vertify whether a tree has branched or not
+    """    
+    return not branches(tree)
+
+def fib_tree(n):
+    if n == 0 or n == 1:
+        return tree(n)
+    else:
+        left, right = fib_tree(n-2), fib_tree(n-1)
+        fib_n = label(left) + label(right)
+        return tree(fib_n, [left, right])
+
+def count_leaves(tree):
+    if is_leaf(tree):
+        return 1
+    else:
+        branch_counts = [count_leaves(b) for b in branches(tree)]
+        return sum(branch_counts)
+
+result = fib_tree(5)
+print(result)
+
+count = count_leaves(fib_tree(5))
+print(count)
